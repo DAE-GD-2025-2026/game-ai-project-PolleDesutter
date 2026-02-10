47cf6f24e278474cfaf87b4266cccb31a1575608
PolleDesutter <61605284+PolleDesutter@users.noreply.github.com>
2026-02-10 14:53:31 +0100

    Added ConstantHelpers
    Added ConstantHelpers:
    -Contains constant variables that are across the whole project

---
+++ Source/GameAIProg/Shared/ConstantHelpers.cpp
@@ -0,0 +1,2 @@
+#include "ConstantHelpers.h"
+
---
+++ Source/GameAIProg/Shared/ConstantHelpers.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+namespace ConstantHelpers
+{
+	constexpr float DebugDefaultLineLength = 150.f;
+	constexpr float	DebugDefaultPointSize = 200.f ;
+	constexpr int	DebugDefaultCircleSegments = 12 ;
+	
+	static FColor DebugDirectionColor = FColor::Magenta;
+	static FColor DebugDirectionLeftColor = FColor::Cyan;
+	
+	static FColor DebugTargetColor = FColor::Red; 
+	
+	
+	
+	
+}
