388e6b539e5092475f8085fd0066360fd5943054
PolleDesutter <61605284+PolleDesutter@users.noreply.github.com>
2026-02-23 20:45:13 +0100

    Added ConstantHelpers Macros
    CLASS_NAME/CLASSNAME_FSTRING
    FUNCTION_NAME/FUNCTION_NAME_FSTRING

--- Source/GameAIProg/Shared/ConstantHelpers.h
+++ Source/GameAIProg/Shared/ConstantHelpers.h
@@ -1,6 +1,17 @@
 #pragma once
 
 
+#include "ConversionHelpers.h"
+#include "UnrealHelpers.h"
+
+
+#define CLASS_NAME		ConversionHelpers::SplitStringView(std::string_view(__FUNCTION__), "::")[0]
+#define FUNCTION_NAME	ConversionHelpers::SplitStringView(std::string_view(__FUNCTION__), "::")[1]
+
+#define CLASS_NAME_FSTRING UnrealHelpers::GetClassNameFromFunction(__FUNCTION__)
+#define FUNCTION_NAME_FSTRING UnrealHelpers::GetFunctionNameFromFunction(__FUNCTION__)
+
+
 namespace ConstantHelpers
 {
 	constexpr float DebugDefaultLineLength = 150.f;
@@ -11,8 +22,7 @@ namespace ConstantHelpers
 	static FColor DebugDirectionLeftColor = FColor::Cyan;
 	
 	static FColor DebugTargetColor = FColor::Red; 
-	
-	
-	
+
 	
 }
+
